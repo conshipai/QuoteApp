6449b6a3e122a28c8bfd210924f0913ebe6ead0a
conshipai <thomas.ehler@conship.ai>
2025-08-30 23:21:35 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: process.env.NODE_ENV || 'production',
   entry: './src/index.js',
   output: {
-    publicPath: 'auto',
+    publicPath: 'https://quotes.gcc.conship.ai/',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   module: {
     rules: [
@@ -17,41 +18,30 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-react'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-    ],
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'quoteapp',
+      name: 'quotes',  // Match the id in your registry
       filename: 'remoteEntry.js',
       exposes: {
-        './App': './src/QuotesModule',
+        './App': './src/QuotesModule'
       },
       shared: {
         react: { singleton: true, requiredVersion: '^18.0.0' },
         'react-dom': { singleton: true, requiredVersion: '^18.0.0' },
-        'react-router-dom': { singleton: true, requiredVersion: '^6.0.0' },
-      },
+        'react-router-dom': { singleton: true, requiredVersion: '^6.0.0' }
+      }
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
+      template: './public/index.html'
+    })
   ],
   resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    port: 3001,
-    hot: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
+    extensions: ['.js', '.jsx']
+  }
 };
